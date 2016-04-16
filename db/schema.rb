540f00ac829c6a5b2b98e4e44f0e43b518ad9789
ggriffin-carecloud <ggriffin@carecloud.com>
2016-04-16 15:36:05 -0400

    rescuing user removal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416145017) do
+ActiveRecord::Schema.define(version: 20160416165057) do
 
   create_table "contact_phone_numbers", force: :cascade do |t|
     t.string "type",            limit: 64,  null: false
@@ -123,10 +123,24 @@ ActiveRecord::Schema.define(version: 20160416145017) do
   add_index "travel_plans", ["start_date", "end_date"], name: "index_travel_plans_on_start_date_and_end_date", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.integer "contact_id", limit: 4,  null: false
-    t.string  "status",     limit: 32, null: false
+    t.integer  "contact_id",             limit: 4,                null: false
+    t.string   "status",                 limit: 32,               null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
   end
 
   add_index "users", ["contact_id"], name: "index_users_on_contact_id", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
