e2d97372c953a7c725267d0768702e48da42d328
Jonathan Rose <jonr87ee@gmail.com>
2016-04-16 17:34:06 -0400

    ADD Visa Direct integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'request#index'
   root 'welcome#index'
+
+  get "request/index" => 'request#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
