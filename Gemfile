a443d2de1f96e027577c805d59fa7855e1847e27
SergiiMiami <sergii.fiot@gmail.com>
2016-04-16 22:00:07 -0400

    google maps api

--- Gemfile
+++ Gemfile
@@ -27,6 +27,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'pakket_hub_core', path: '../pakket_hub_core'
 
+gem 'rest-client'
+
+gem 'json'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ app/controllers/coordinates_controller.rb
@@ -0,0 +1,25 @@
+class CoordinatesController < ApplicationController
+
+  def create
+  	address1 = params[:address1]
+  	address2 = params[:address2]
+  	response1 = RestClient.get "https://maps.googleapis.com/maps/api/geocode/json",
+  	 {:params => {'address' => address1,
+  	 	 		  'key'=>'AIzaSyDazU0gvbBdl2aKSaBUMNHkqG6uVLYW7jI'}}
+  	response2 = RestClient.get "https://maps.googleapis.com/maps/api/geocode/json",
+  	 {:params => {'address' => address2,
+  	 	 		  'key'=>'AIzaSyDazU0gvbBdl2aKSaBUMNHkqG6uVLYW7jI'}}
+
+	lat1 = JSON.parse(response1)["results"][0]["geometry"]["location"]["lat"]
+	lng1 = JSON.parse(response1)["results"][0]["geometry"]["location"]["lng"]
+
+	lat2 = JSON.parse(response2)["results"][0]["geometry"]["location"]["lat"]
+	lng2 = JSON.parse(response2)["results"][0]["geometry"]["location"]["lng"]
+
+	@distance = (Math.sqrt((lat2-lat1)*(lat2-lat1)+(lng2-lng1)*(lng2-lng1))*69).round(1)
+	@lat_middle = ((lat1 + lat2) / 2).round(5)
+	@lng_middle = ((lng1 + lng2) / 2).round(5)
+	render :show
+  end
+  
+end
